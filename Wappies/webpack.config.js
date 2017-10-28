2e42d4b8672363e3630084ac25b055663c5d5842
Josh Green <mrjoshgreen90@gmail.com>
2017-10-28 19:38:18 +0100

    :goat: Adding SASSyness

--- Wappies/webpack.config.js
+++ Wappies/webpack.config.js
@@ -19,7 +19,14 @@ module.exports = (env) => {
             rules: [
                 { test: /\.ts$/, include: /ClientApp/, use: isDevBuild ? ['awesome-typescript-loader?silent=true', 'angular2-template-loader'] : '@ngtools/webpack' },
                 { test: /\.html$/, use: 'html-loader?minimize=false' },
-                { test: /\.css$/, use: [ 'to-string-loader', isDevBuild ? 'css-loader' : 'css-loader?minimize' ] },
+                { test: /\.css$/, use: ['to-string-loader', isDevBuild ? 'css-loader' : 'css-loader?minimize'] },
+                {
+                    test: /\.s[ac]ss$/,
+                    exclude: /node_modules/,
+                    use: [
+                        'raw-loader',
+                        'sass-loader']
+                },
                 { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
             ]
         },
