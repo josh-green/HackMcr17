fda8fef32e3049d463d4dcd3f00bc31529b3ffb5
Adam Chappell <admchppll@gmail.com>
2017-10-28 18:11:51 +0100

    :goat: Initial upload of the data models for the basic user, administrator, report (each S.O.S trigger is a report) and location

---
+++ Wappies/Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wappies.Models
+{
+    public class Location
+    {
+        [DataType(DataType.DateTime)]
+        DateTime DateTime;
+
+        String Longitude;
+
+        String Latitude;
+    }
+}
