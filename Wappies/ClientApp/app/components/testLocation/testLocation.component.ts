31e7c9b1b98b466ed171e9239c48101d4af864ed
Josh Green <mrjoshgreen90@gmail.com>
2017-10-28 22:57:53 +0100

    :goat: location.

--- Wappies/ClientApp/app/components/testLocation/testLocation.component.ts
+++ Wappies/ClientApp/app/components/testLocation/testLocation.component.ts
@@ -1,15 +1,36 @@
 ﻿import { Component, OnInit } from '@angular/core';
 
+
 @Component({
     selector: 'testLocation',
     templateUrl: './testLocation.component.html',
     styleUrls: ['./testLocation.component.scss']
 })
 /** testLocation component*/
-export class TestLocationComponent implements OnInit
-{
+export class TestLocationComponent implements OnInit {
     /** testLocation ctor */
     constructor() { }
+
+    locations: string[] = [];
+
+    getLocation(): any {
+        if (!this.isAvailable()) {
+            console.error('navigator.geolocation is unavailable');
+            return;
+        }
+
+        navigator.geolocation.getCurrentPosition((position) => {
+                this.locations.push(position.coords.latitude + ', ' + position.coords.longitude);
+
+            },
+            (err) => {
+                console.error('A geolocation error occurred.', err);
+            });  
+    };
+
+    isAvailable(): boolean {
+        return !!('geolocation' in navigator);
+    }
 
     /** Called by Angular after testLocation component initialized */
     ngOnInit(): void { }
