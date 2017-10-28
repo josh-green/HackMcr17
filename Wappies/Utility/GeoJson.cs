007460c3927f8069d190014c4c342151c97d98ea
Adam Chappell <admchppll@gmail.com>
2017-10-29 00:58:30 +0100

    :goat: Started the Client API endpoints. Created a user utility (may not needed if we can't get the PWA to pass the phone number). Also adding GeoJson class (sorry i forgot this Brodie)

---
+++ Wappies/Utility/GeoJson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wappies.Utility
+{
+    public class GeoJson
+    {
+        public string Latitude;
+        public string Longitude;
+        public string Message;
+
+        public GeoJson(string Lat, string Long, string Msg) {
+            Latitude = Lat;
+            Longitude = Long;
+            Message = Msg;
+        }
+    }
+}
---
+++ Wappies/Utility/UserUtility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wappies.Models;
+using Wappies.Context;
+
+namespace Wappies.Utility
+{
+    public class UserUtility
+    {
+        public static User CreateUser(string Phone) {
+            User user;
+            using (DatabaseContext db = new DatabaseContext()) {
+                try
+                {
+                    user = db.Users.Where(u => u.PhoneNumber == Phone).Single();
+                }
+                catch (System.InvalidOperationException) {
+                    user = new User();
+                    user.PhoneNumber = Phone;
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                }
+            }
+            return user;
+        }
+    }
+}
