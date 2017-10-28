465a53b5a1876d4df324b27f1067dff89ee3c81d
Adam Chappell <admchppll@gmail.com>
2017-10-28 20:34:10 +0100

    :goat: additional data annotations for the model to better enable entity framework

--- Wappies/Models/Report.cs
+++ Wappies/Models/Report.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,17 @@ namespace Wappies.Models
 {
     public class Report
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         int ID;
 
         [DataType(DataType.DateTime)]
         DateTime Created;
 
         bool Completed;
+
+        [ForeignKey("User")]
+        int UserID;
 
         User User;
 
