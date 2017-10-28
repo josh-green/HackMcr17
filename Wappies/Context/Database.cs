28993b9834131cc741dcaf46b5edd068bca944ad
Adam Chappell <admchppll@gmail.com>
2017-10-28 21:20:27 +0100

    :goat: added the database context for the entity framework

---
+++ Wappies/Context/Database.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wappies.Models;
+
+namespace Wappies.Context
+{
+    public class Database : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Administrator> Administrators { get; set; }
+        public DbSet<Report> Reports { get; set; }
+        public DbSet<Location> Locations { get; set; }
+    }
+}
