492117aea602c2f9d2d85adefcb350ecdaba413d
Brodie Gilmour <brodiegilmour@hotmail.co.uk>
2017-10-28 16:40:31 +0100

    Adding map tab.

---
+++ Wappies/ClientApp/app/components/map/map.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { mapboxgl } from 'mapbox-gl';
+
+@Component({
+    selector: 'map',
+    templateUrl: './map.component.html'
+})
+export class HomeComponent {
+    public static map: any; 
+
+    constructor() {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiYnJ1ZGRsZXMiLCJhIjoiY2lmOXA1M2hoMDAyaHVja25pYjBnbXl0aCJ9.CT8iLt0HbViwX6ktZhpIzQ';
+        HomeComponent.map = new mapboxgl.Map({
+            container: 'mapbox-map',
+            style: 'mapbox://styles/mapbox/streets-v9'
+        });
+
+    }
+}
--- Wappies/webpack.config.vendor.js
+++ Wappies/webpack.config.vendor.js
@@ -21,6 +21,7 @@ const nonTreeShakableModules = [
     'es6-shim',
     'event-source-polyfill',
     'jquery',
+    'mapbox-gl'
 ];
 const allModules = treeShakableModules.concat(nonTreeShakableModules);
 
