ac6ffe2481371513f9167d0eb91dd3ac12893cab
Josh Green <mrjoshgreen90@gmail.com>
2017-10-29 01:23:35 +0000

    :goat: Fixed location provider with Http injection

--- Wappies/ClientApp/app/services/location/location.service.provider.ts
+++ Wappies/ClientApp/app/services/location/location.service.provider.ts
@@ -1,12 +1,13 @@
 ﻿import { LocationService } from './location.service';
+import { Http } from '@angular/http';
 
-let locationServiceFactory = () => {
-    return new LocationService();
+let locationServiceFactory = (http: Http) => {
+    return new LocationService(http);
 };
 
 export let locationServiceProvider =
 {
     provide: LocationService,
     useFactory: locationServiceFactory,
-    deps: []
+    deps: [Http]
 };
